7626cd91da619e64c3fa66220b86d5f348364f13
Sejin <sejigner@gmail.com>
2024-08-19 23:07:32 +0900

    upload all files

---
+++ chat.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from dotenv import load_dotenv
+from llm import get_ai_response
+
+st.set_page_config(page_title="소득세 챗봇", page_icon="🤖")
+
+st.title("🤖 소득세 챗봇")
+st.caption("소득세에 관련된 모든 것")
+
+load_dotenv()
+
+if 'message_list' not in st.session_state:
+    st.session_state.message_list = []
+
+for message in st.session_state.message_list:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+
+if user_question := st.chat_input(placeholder="소득세와 관련된 질문을 해주세요."):
+    with st.chat_message("user"):
+        st.write(user_question)
+    st.session_state.message_list.append({"role": "user", "content": user_question})
+
+    with st.spinner("답변을 생성하고 있습니다."):
+        ai_response = get_ai_response(user_question)
+
+        with st.chat_message("ai"):
+            ai_message = st.write_stream(ai_response)
+        st.session_state.message_list.append({"role": "ai", "content": ai_message})
